f5071ed2079179ae7763ec98092230ad708a8cca
Laith Mimi <laithmimi03@gmail.com>
2025-05-07 11:19:15 +0300

    trigger icon

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -16,14 +16,16 @@ const SelectTrigger = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <SelectPrimitive.Trigger
     ref={ref}
+    dir="ltr" 
     className={cn(
       "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
-    )}
-    dir="auto"
+    )} 
     {...props}
   >
+    <span dir="auto">
     {children}
+    </span>
     <SelectPrimitive.Icon asChild>
       <ChevronDown className="h-4 w-4 opacity-50" />
     </SelectPrimitive.Icon>
